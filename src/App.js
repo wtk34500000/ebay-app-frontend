d5f7fcd392acbc75124d60fb24752453b33f9ff2
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-04-19 08:52:55 -0400

    again

--- src/App.js
+++ src/App.js
@@ -50,9 +50,9 @@ class App extends Component {
               <Route  path ={`${url}/cart/checkout`} component={PaymentForm} />
               <Route  path ={`${url}/cart`} component={Cart} />
               <Route  path ={url} component={HomeContainer} />
-              <Route  path ='/login' component={Login} />
-              <Route  path ='/signup' component={Signup} />
-              <Route  path ='/' component={LandingPage} />
+              <Route  path ={`${url}/login`} component={Login} />
+              <Route  path ={`${url}/signup`} component={Signup} />
+              {/* <Route  path ='/' component={LandingPage} /> */}
         </Switch>
       </div>
     );
